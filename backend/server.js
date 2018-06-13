f7899d55515324f59cf98be24fa85eaf03b18079
sundara0rajan <sundara_rajan@yahoo.com>
2018-06-13 16:15:11 +0200

    Update server.js
    Done

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.get('/api/me', (request, response) => {
 });
 
 app.get('/api/work', (request, response) => {
-	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
+	response.send('[{"yearFrom": "2011", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
